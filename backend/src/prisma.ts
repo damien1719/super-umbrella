48f37117ab54f454117a1e4201e56a78f0775e0f
damien1719 <54616022+damien1719@users.noreply.github.com>
2025-06-12 14:48:45 +0200

    [backend] add article CRUD structure with validation

--- backend/src/prisma.ts
+++
@@ -1,7 +0,0 @@
-// src/prisma.ts
-import { PrismaClient } from '@prisma/client'
-export const prisma = new PrismaClient({
-  datasourceUrl: process.env.NODE_ENV === 'production'
-      ? process.env.DATABASE_URL_POOL
-      : process.env.DATABASE_URL
-})
