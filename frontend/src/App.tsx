a1a3073ad18786ef2a8800901b76d203a58f8275
Mossuz <damien.mossuz@kili-technology.com>
2025-06-17 05:54:49 +0200

    cleaning sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import AppSidebar from './components/SideBar/AppSidebar';
-import { SidebarProvider, SidebarInset } from './components/ui/sidebar';
 import Dashboard from './pages/Dashboard';
 import MesBiens from './pages/MesBiens';
 import Agenda from './pages/Agenda';
@@ -12,36 +10,8 @@ import type { Page } from './store/pageContext';
 
 
 export default function App() {
-  const [activeSection, setActiveSection] = useState("MesBiens")
-  const [isProfileDialogOpen, setIsProfileDialogOpen] = useState(false)
 
-  const renderActiveSection = () => {
-    switch (activeSection) {
-      case "Dashboard":
-        return <Dashboard />
-      case "MesBiens":
-        return <MesBiens />
-      case "Agenda":
-        return <Agenda />
-      case "Abonnement":
-        return <Abonnement />
-      default:
-        return <MonCompte />
-    }
   }
 
   return (
-    <SidebarProvider>
-      <AppSidebar
-        activeSection={activeSection}
-        onSectionChange={setActiveSection}
-        onProfileEdit={() => setIsProfileDialogOpen(true)}
-      />
-      <SidebarInset>
-        <main className="flex-1 space-y-4 p-8 pt-6">{renderActiveSection()}</main>
-      </SidebarInset>
-
-
-    </SidebarProvider>
-  )
 }
