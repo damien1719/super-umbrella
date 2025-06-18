30e71012593ab30ee6ab6de9b028923e38f4d83c
damien1719 <54616022+damien1719@users.noreply.github.com>
2025-06-18 20:57:31 +0200

    [backend] add tenant CRUD and property route

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -4,6 +4,8 @@ export type NewBien = Prisma.BienCreateInput;
 export type EditBien = Prisma.BienUpdateInput;
 export type NewLocation = Prisma.LocationCreateInput;
 export type EditLocation = Prisma.LocationUpdateInput;
+export type NewLocataire = Prisma.LocataireCreateInput;
+export type EditLocataire = Prisma.LocataireUpdateInput;
 
 export * from './types/UserProfile';
 export * from './types/ApiResponse';
