31de103a550c88b48ef463ce64a03df77fa872d0
damien1719 <54616022+damien1719@users.noreply.github.com>
2025-08-08 09:45:49 +0200

    Revert "[backend] Support multi-provider auth"

--- backend/tests/setup.ts
+++ backend/tests/setup.ts
@@ -1,16 +1,2 @@
 import '@prisma/client';
 jest.mock('@prisma/client');
-
-jest.mock('jose', () => ({
-  createRemoteJWKSet: jest.fn(),
-  jwtVerify: jest.fn(),
-}), { virtual: true });
-
-jest.mock(
-  'openai',
-  () => ({
-    __esModule: true,
-    default: class {},
-  }),
-  { virtual: true }
-);
